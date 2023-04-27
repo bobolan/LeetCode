93fd958fc35c18ed55b64f78f7fede0f93d3dc82
Job <2285665973@qq.com>
2023-04-27 23:02:45 +0800

    买卖股票的最佳时机 II---贪心解法

--- leetCode/src/com/leetCode/easy/Solution1.java
+++ leetCode/src/com/leetCode/easy/Solution1.java
@@ -29,11 +29,23 @@ package com.leetCode.easy;
  */
 public class Solution1 {
     public static void main(String[] args) {
+        int[] prices = new int[] {7,1,5,3,6,4};
+        int profit = maxProfit(prices);
+        System.out.println("profit:" + profit);
 
     }
 
+    /**
+     * 贪心算法
+     * @param prices
+     * @return
+     */
     public static int maxProfit(int[] prices) {
-        // TODO something
-        return 0;
+        if (prices == null && prices.length<2) return 0;
+        int count = 0;
+        for (int i = 1; i <prices.length; i++) {
+            count += Math.max(prices[i]-prices[i-1], 0);
+        }
+        return count;
     }
 }
